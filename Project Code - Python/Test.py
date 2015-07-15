9f3d07bf7c737bc7f8b63da08fe5a116bc860e29
jlouie6 <jlouie6@gatech.edu>
2015-07-15 14:09:48 -0400

    Project 3 update

--- Project Code - Python/Test.py
+++ Project Code - Python/Test.py
@@ -1,12 +1,79 @@
 from Figure import Figure
+from PIL import Image
 from Object import Object
+import copy
 
+def image_and(im1, im2):
+    if im1.size != im2.size:
+        raise Exception('Images must be same size to AND them')
+    size = im1.size
+    image = Image.new('L', size, color=255)
+
+    for x in range(size[0]):
+        for y in range(size[1]):
+            xy = x, y
+            if im1.getpixel(xy) == im2.getpixel(xy) == 0:
+                image.putpixel(xy, 0)
+    return image
+
+def image_xor(im1, im2):
+    if im1.size != im2.size:
+        raise Exception('Images must be same size to XOR them')
+    size = im1.size
+    image = Image.new('L', size, color=255)
+
+    for x in range(size[0]):
+        for y in range(size[1]):
+            xy = x, y
+            if im1.getpixel(xy) != im2.getpixel(xy):
+                image.putpixel(xy, 0)
+    return image
+
+def figure_add(fig1, fig2):
+    if fig1.image.size != fig2.image.size:
+        raise Exception('Figures must be same size to SUBTRACT them')
+
+    size = fig1.image.size
+    image = Image.new('L', size, color=255)
+
+    for obj1 in fig1.objects:
+        for xy in obj1.area:
+            image.putpixel(xy, 0)
+
+    for obj2 in fig2.objects:
+        for xy in obj2.area:
+            image.putpixel(xy, 0)
+
+    return image
+
+def figure_subtract(fig1, fig2):
+    if fig1.image.size != fig2.image.size:
+        raise Exception('Figures must be same size to SUBTRACT them')
+    size = fig1.image.size
+    image = Image.new('L', size, color=255)
+
+    for x in range(size[0]):
+        for y in range(size[1]):
+            xy = x, y
+            if im2.getpixel(xy) == 0:
+                image.putpixel(xy, 255)
+    return image
+
+fig_e10_a = Figure("E-10-A.png")
+fig_e10_b = Figure("E-10-B.png")
+
+image_and(fig_e10_a.image, fig_e10_b.image).show()
+image_xor(fig_e10_a.image, fig_e10_b.image).show()
+image_add(fig_e10_b.image, fig_e10_a.image).show()
+image_subtract(fig_e10_b.image, fig_e10_a.image).show()
+
+'''
 fig1 = Figure("1.png")
 fig7 = Figure("7.png")
 fig8 = Figure("8.png")
 
-fig_a = Figure("Problems/Basic Problems D/Basic Problem D-02/A.png")
-fig_b = Figure("Problems/Basic Problems D/Basic Problem D-02/B.png")
+fig_a = Figure("Problems/Basic Problems D/Basic Problem D-02/E-10-A.png")
+fig_b = Figure("Problems/Basic Problems D/Basic Problem D-02/E-10-B.png")
 fig_c = Figure("Problems/Basic Problems D/Basic Problem D-02/C.png")
 fig_d = Figure("Problems/Basic Problems D/Basic Problem D-02/D.png")
 fig_e = Figure("Problems/Basic Problems D/Basic Problem D-02/E.png")
@@ -28,4 +95,6 @@ if fig_a.objects[0] == fig_e.objects[0]:
 
 if fig_a.objects[0] == fig_b.objects[0]:
     print 'A and B are equal'
+'''
+
 
